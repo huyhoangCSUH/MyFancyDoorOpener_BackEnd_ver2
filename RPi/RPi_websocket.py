90759e4aa0b5c2ff2c0d80f7e78768f174f938a4
Huy Hoang <huyhoang@Huys-MacBook-Pro.local>
2017-11-21 23:24:07 -0600

    minor change

--- RPi/RPi_websocket.py
+++ RPi/RPi_websocket.py
@@ -1,11 +1,12 @@
 from websocket import create_connection
 host = raw_input("host: ")
 port = 9998
-ws = create_connection("ws://" + host + str(port))
-print "Asking for auth code"
-ws.send("Auth code asked")
-print "Sent"
-print "Waiting for answer..."
-result = ws.recv()
-print "Auth received!"
-ws.close()
+while 1:
+    ws = create_connection("ws://" + host + str(port))
+    print "Connection established. Asking for auth code!"
+    ws.send("Auth code asked")
+    print "Sent"
+    print "Waiting for answer..."
+    result = ws.recv()
+    print "Auth received! " + str(result)
+    ws.close()
--- server_backend/server_websocket.py
+++ server_backend/server_websocket.py
@@ -1,16 +1,20 @@
 from SimpleWebSocketServer import SimpleWebSocketServer, WebSocket
 import time
+import os
+
+userhome = os.path.expanduser('~')
+path_for_files = userhome + "/MyFancyDoorOpener_BackEnd_ver2/server_backend"
 
 class SimpleEcho(WebSocket):
 
     def handleMessage(self):
         while 1:
             time.sleep(1.0)
-            with open('input.txt') as f:
+            with open(path_for_files + '/auth_stat.txt') as f:
                 auth_code = f.read().strip()
             if auth_code == '1':
                 break
-        # echo message back to client
+        # send authorization to client
         self.sendMessage(auth_code)
 
     def handleConnected(self):
